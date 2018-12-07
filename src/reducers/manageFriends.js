5c79d21cb6b78dc5ebc6b4149296a2bce1e56789
soohoo05 <tyler.soohoo@gmail.com>
2018-12-06 21:37:49 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,8 +1,24 @@
 export function manageFriends(state, action){
-  if(action.type === "ADD_FRIEND"){
-    return {...state, friend:"Chrome Boi"}
-  }
-  else{
-    return state
+  switch (action.type) {
+    case 'ADD_FRIEND':
+        return {
+          "friends": [
+            ...state.friends,
+            action.friend
+          ]
+        }
+      break;
+    case 'REMOVE_FRIEND':
+        return {
+          "friends": [
+            ...state.friends.filter(friend => {
+              return friend.id !== action.id
+            })
+          ]
+        }
+        break;
+    default:
+        return state
+      break;
   }
 }
